81ed114520137f1c51c8b05d078cfaac9ffa2ff5
issach315 <issachemani227@gmail.com>
2026-04-17 21:24:28 +0530

    project initiated

---
+++ src/main/java/com/clientmanagement/ClientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientmanagement/ClientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
